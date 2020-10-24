22bc22599876d541fca7407a59ea93fb967c544e
Douglas Sigmon <douglas.sigmon@gmail.com>
2020-10-23 19:02:30 -0600

    full stack frame

---
+++ CustomerCrud/src/main/java/com/skilldistillery/jpacrud/controllers/CustomerController.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.jpacrud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomerController {
+	@RequestMapping(path= "/")
+	public String index() {
+		return "index";
+	}
+}
